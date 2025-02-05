9e033e29fdc632d3fe1c14ebc8e0cea68b6a1ba1
sparklebutt <arandomsanti@gmail.com>
2025-02-05 20:12:52 +0200

    fix, m9 ex02, insert algorithm for vec, lst added

--- M9/ex02/PmergeMe.cpp
+++ M9/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: shaboom <shaboom@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 10:49:19 by shaboom           #+#    #+#             */
-/*   Updated: 2025/02/05 15:01:45 by shaboom          ###   ########.fr       */
+/*   Updated: 2025/02/05 20:10:06 by shaboom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,17 @@ static void printContainer(const Container& cont) {
 }
 void PmergeMe::test()
 {
-	std::cout<<"bug hunting 000\n";
+	//TimeDiff1
+	std::cout<<"insertion sort vecotr\n";
+	//time stamp start
 	insertionSortVec(m_vec, 0, m_vec.size() - 1);
-	std::cout<<"bug hunting \n";
 	printContainer(m_vec);
+	//time stamp end
+	// take comparison time store in timeDiff1 
+	insertionSortList(m_lst);
+	printContainer(m_lst);
+	// new start time
+	std::cout<<"insertion sort deque\n";
 } 
 PmergeMe::PmergeMe() {std::cout<<"default constructor called\n";}
 
@@ -35,7 +42,7 @@ PmergeMe::PmergeMe(const std::string& set) {
 
 	//fill vector and deq
 	fillContainers(set);
-	//printContainer(m_deq);
+	//printContainer(m_lst);
 	//printContainer(m_vec);
 	std::cout<<"default constructor called\n";
 }
@@ -53,7 +60,7 @@ void PmergeMe::fillContainers(const std::string& set)
 		{
 			convertedNum = stoi(num);
 			// if (convertedNum > or something) throw
-			m_deq.push_back(convertedNum);
+			m_lst.push_back(convertedNum);
 			m_vec.push_back(convertedNum);
 		}
 		catch(const std::exception& e)
@@ -64,33 +71,116 @@ void PmergeMe::fillContainers(const std::string& set)
 }
 PmergeMe::~PmergeMe() {std::cout<<"deconstructor called\n";}
 
-void PmergeMe::insertionSortVec(std::vector<unsigned int>& vec, unsigned int left, unsigned int right) {
-    //std::cout<<"bug hunting 111\n";
-	if (vec.empty())
+/**
+ * @brief bit shifting here by 1 is the same as / 2
+ * 
+ * @param vec 
+ * @param key 
+ * @param left 
+ * @param right 
+ * @param comp 
+ * @return unsigned int 
+ */
+template<typename Container>
+int PmergeMe::insertPoint(const Container& cont, unsigned int key, int left, int right, int& comp) {
+    while (left <= right) {
+        comp++;
+//		std::cout<<"when is right and out of bounds matching = "<<right<<"and left "<<left<<std::endl;
+		//unsigned int mid = left + (right - left) / 2;
+		unsigned int mid = left + ((right - left) >> 1);
+        if (cont[mid] == key)
+		{
+			comp++;
+            return mid + 1;			
+		}
+        else if (cont[mid] < key)
+		{
+			comp++;
+            left = mid + 1;
+		}
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
+/**
+ * @brief Insertion sort is an algorithm that builds the sorted array one element at a time 
+ * by taking each new element and inserting it into its correct position within the already sorted portion.
+ * It repeatedly compares and shifts elements until the new element is in its proper place. Random placement would
+ * for example no longer be an insertion sort. 
+ * 
+ * comparisons are any comparison in the sorting process. 
+ * 
+ * @param vec 
+ * @param left 
+ * @param right 
+ * 	if (vec.empty())
 		throw std::runtime_error("vec was empty for some reason ");
 	if (right < left)
 		throw std::runtime_error("right is bigger than ölefty  ");
-	for (unsigned int i = left + 1; i <= right; ++i) {
-	    unsigned int key = vec[i];
-		std::cout<<"key to insert = "<<key<<"\n";
+ */
+
+void PmergeMe::insertionSortVec(std::vector<unsigned int>& vec, int left, int right) {
+	int comp = 0;
+
+	for (int i = left + 1; i <= right; ++i) {
+	    comp++;
+		int key = vec[i];
+		//std::cout<<"key to insert = "<<key<<"\n";
         int j = i - 1;
-	    while (j >= static_cast<int>(left) && vec[j] > key) {
-            vec[j + 1] = vec[j];
-			std::cout << "Shifting: ";
-            for (const auto& num : vec) {
-                std::cout << num << " ";
-            }
-            std::cout << std::endl;
+		//std::cout<<"when is j and out of bounds matching = "<<j<<"and left "<<left<<std::endl;
+	    int pos = insertPoint(vec, key, left, j, comp);
+		while (j >= pos) {
+		    vec[j + 1] = vec[j];
 			--j;
+			comp++;
         }
         vec[j + 1] = key;
-		std::cout << "Array after inserting key: ";
+		/*std::cout << "Array after inserting key: ";
         for (const auto& num : vec) {
             std::cout << num << " ";
         }
-        std::cout << std::endl;
+        std::cout << std::endl;*/
+        std::cout <<"comp count = "<<comp<< std::endl;
     }
 }
+
+
+void PmergeMe::insertionSortList(std::list<unsigned int>& lst) {
+	int comp = 0;
+	
+    //if (lst.empty()) return; this check should happen before this fucntion 
+    std::list<unsigned int>::iterator it = lst.begin();
+    ++it;
+    for (; it != lst.end(); ++it) {
+        unsigned int key = *it;
+		comp++;
+        // Move backwards to find the correct position for 'key'
+        std::list<unsigned int>::iterator insertionPoint = lst.begin();
+        while (insertionPoint != it && *insertionPoint <= key) {
+			comp += 2;
+		    ++insertionPoint;
+        }        
+        // Shift elements and insert 'key' at the correct position
+        std::list<unsigned int>::iterator k = it;
+        while (k != insertionPoint) {
+			comp++;
+		    *k = *std::prev(k);
+            --k;
+        }
+        *insertionPoint = key;
+        // Debug output
+        /*std::cout << "List after inserting key: ";
+        for (const auto& num : lst) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+		}*/
+	}
+    std::cout <<"comp count list= "<<comp<< std::endl;
+}
+
 // Helper function to insert in sorted order
 /*template<typename Container>
 void insertInOrder(Container& cont, typename Container::iterator start, typename Container::iterator end) {
--- M9/ex02/PmergeMe.hpp
+++ M9/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: shaboom <shaboom@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 10:24:27 by shaboom           #+#    #+#             */
-/*   Updated: 2025/02/05 14:17:37 by shaboom          ###   ########.fr       */
+/*   Updated: 2025/02/05 19:57:52 by shaboom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <string>
 #include <exception>
 #include <vector>
-#include <deque>
+#include <list>
 /**
  * @brief do we leave room for 2 fucntions, one to check string clean, then stoi
  * 
@@ -24,10 +24,20 @@
 class PmergeMe
 {
 	private:
-		std::deque<unsigned int> m_deq;
+		std::list<unsigned int> m_lst;
 		std::vector<unsigned int> m_vec;
 		void fillContainers(const std::string& set);
-		void insertionSortVec(std::vector<unsigned int>& vec, unsigned int left, unsigned int right);
+		// helper fucntion for insertion 
+		template <typename Container>
+		int insertPoint(const Container& cont, unsigned int key, int left, int right, int& comp);
+		//insertion
+		void insertionSortVec(std::vector<unsigned int>& vec, int left, int right);
+		//void insertionSortDeque(std::deque<unsigned int>& deq);
+		void insertionSortList(std::list<unsigned int>& lst);
+		// helper fucntion for insertion 
+		//int insertPointVec(const std::vector<unsigned int>& vec, unsigned int key, int left, int right, int& comp);
+		//insertion
+		//void insertionSortVec(std::vector<unsigned int>& vec, int left, int right);
 
 	public:
 		PmergeMe ();
